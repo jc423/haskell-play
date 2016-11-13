c6da70f58b55a11ae0fa4ddcb8e4a4f8723be13b
Crawford, Joseph (CORP) <Joseph.Crawford@adp.com>
2016-11-12 20:41:34 -0500

    checking counts of positions

--- fantasy-football.hs
+++ fantasy-football.hs
@@ -4,6 +4,13 @@ data Player = Player { position :: Position
                      , name :: String
                      } deriving (Show)
 
-team :: Player -> Player -> [Player]
-team x y | position x /= position y = [x, y]
-         | otherwise = [x]
+positionCount :: [Player] -> Position -> Int
+positionCount players pos = length $ filter (\x -> position x == pos) players
+
+team :: Player -> Player -> Player -> Player -> Player -> Player -> Player -> [Player]
+team a b c d e f g  | validQbCount && validRBCount && validWRCount = a:b:c:d:e:f:g:[]
+                    | otherwise = error "invalid team"
+                    where playerList = a:b:c:d:e:f:g:[]
+                          validQbCount = positionCount playerList QB == 1
+                          validRBCount = positionCount playerList RB == 2
+                          validWRCount = positionCount playerList WR == 2
