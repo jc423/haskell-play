152a6c079170fa8e560a9b44194208fe8759ed62
Crawford, Joseph (CORP) <Joseph.Crawford@adp.com>
2016-11-14 00:14:24 -0500

    validated team with flex

--- fantasy-football.hs
+++ fantasy-football.hs
@@ -1,16 +1,32 @@
-data Position = QB | RB | WR | TE deriving (Show, Eq)
+import qualified Data.Map as Map
+
+data Position = QB | RB | WR | TE deriving (Show, Eq, Ord)
 
 data Player = Player { position :: Position
                      , name :: String
                      } deriving (Show)
 
+type PositionCountMap = Map.Map Position Int
+
 positionCount :: [Player] -> Position -> Int
 positionCount players pos = length $ filter (\x -> position x == pos) players
 
-team :: Player -> Player -> Player -> Player -> Player -> Player -> Player -> [Player]
-team a b c d e f g  | validQbCount && validRBCount && validWRCount = a:b:c:d:e:f:g:[]
-                    | otherwise = error "invalid team"
-                    where playerList = a:b:c:d:e:f:g:[]
-                          validQbCount = positionCount playerList QB == 1
-                          validRBCount = positionCount playerList RB == 2
-                          validWRCount = positionCount playerList WR == 2
+getCountsForPositions :: [Player] -> PositionCountMap
+getCountsForPositions playerList = Map.fromList (map (\x -> (x, positionCount playerList x)) [QB, RB, WR, TE])
+
+
+team :: [Player] -> [Player]
+team xs  | validQbCount && validRBCount && validWRCount && validTECount && length xs == 7 = xs
+         | otherwise = error "invalid team"
+         where positionCounts = getCountsForPositions xs
+               validQbCount = Map.lookup QB positionCounts == Just 1
+               validRBCount = Map.lookup RB positionCounts >= Just 2
+               validWRCount = Map.lookup WR positionCounts >= Just 2
+               validTECount = Map.lookup TE positionCounts >= Just 1
+
+qb = Player{position=QB, name="joe"}
+rb = Player{position=RB, name="thurman"}
+rb2 = Player{position=RB, name="marshawn"}
+wr = Player{position=WR, name="andre"}
+wr2 = Player{position=WR, name="lee"}
+te = Player{position=TE, name="scott"}
